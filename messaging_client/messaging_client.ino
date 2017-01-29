ae08cc7dcef15c2b33e25ee17e309c44231262f1
thomasshouler <thomas.shouler@gmail.com>
2017-01-29 13:40:48 -0600

    Reformat sketch

--- messaging_client/messaging_client.ino
+++ messaging_client/messaging_client.ino
@@ -2,28 +2,26 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
 byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 IPAddress messageQueueIP(10, 0, 1, 4);
 int messageQueuePort = 1883;
 char messageQueueUsername[] = "noah";
 char messageQueuePassword[] = "herron";
 
-EthernetClient ethClient;
-PubSubClient client(ethClient);
+EthernetClient ethernetClient;
+PubSubClient pubSubClient(ethernetClient);
 
 void reconnect() {
-  while (!client.connected()) {
+  while (!pubSubClient.connected()) {
     Serial.print("Attempting connection with message queue... ");
-    if (client.connect("arduinoClient", messageQueueUsername, messageQueuePassword)) {
+    if (pubSubClient.connect("arduinoClient", messageQueueUsername, messageQueuePassword)) {
       Serial.println("connected");
-      client.publish("outTopic","hello world");
+      pubSubClient.publish("outTopic","hello world");
       Serial.println("Successfully published message");
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+      Serial.print(pubSubClient.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -38,20 +36,17 @@ void setup()
   Serial.print("connected with local IP ");
   Serial.println(Ethernet.localIP());
   
-  client.setServer(messageQueueIP, messageQueuePort);
+  pubSubClient.setServer(messageQueueIP, messageQueuePort);
   Serial.print("Arduino configured to consume message queue at ");
   Serial.print(messageQueueIP);
   Serial.print(":");
   Serial.println(messageQueuePort);
-  
-  // Allow the hardware to sort itself out
-  delay(1500);
 }
 
 void loop()
 {
-  if (!client.connected()) {
+  if (!pubSubClient.connected()) {
     reconnect();
   }
-  client.loop();
+  pubSubClient.loop();
 }
