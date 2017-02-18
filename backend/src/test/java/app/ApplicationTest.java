97c5572c316542638c16abe1fc504f8a047b8024
thomasshouler <thomas.shouler@gmail.com>
2017-02-18 11:56:23 -0600

    Use better annotation for app test

--- backend/src/test/java/app/ApplicationTest.java
+++ backend/src/test/java/app/ApplicationTest.java
@@ -2,12 +2,10 @@ package app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@EnableAutoConfiguration
-@Configuration
+@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ApplicationTest {
 
--- mock-messaging-client/src/test/java/app/ApplicationTest.java
+++ mock-messaging-client/src/test/java/app/ApplicationTest.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@EnableAutoConfiguration
-@Configuration
+@SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ApplicationTest {
 
