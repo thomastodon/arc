d56cd1bcd73840129d6c79abfcdfac118092bfcb
thomasshouler <thomas.shouler@gmail.com>
2017-07-09 14:29:05 -0500

    Simplify CORS config

--- backend/src/main/kotlin/app/WebConfig.kt
+++ backend/src/main/kotlin/app/WebConfig.kt
@@ -3,14 +3,13 @@ package app
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-open class WebConfig : WebMvcConfigurerAdapter() {
+open class WebConfig : WebMvcConfigurer {
 
-    //    TODO: nullable registry?
-    override fun addCorsMappings(registry: CorsRegistry?) {
-        registry!!.addMapping("/**")
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
     }
 }
