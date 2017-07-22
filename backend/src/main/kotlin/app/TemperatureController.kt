e16bd1c12a371e855eee70645aea9ae2bbd827d5
thomasshouler <thomas.shouler@gmail.com>
2017-07-22 15:59:21 -0500

    Better handle client disconnections

--- backend/src/main/kotlin/app/TemperatureController.kt
+++ backend/src/main/kotlin/app/TemperatureController.kt
@@ -1,18 +1,23 @@
 package app
 
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE
 import org.springframework.messaging.MessageHandler
 import org.springframework.messaging.SubscribableChannel
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
+import java.io.IOException
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/temperatures")
 open class TemperatureController(
     private val serverSentEventChannel: SubscribableChannel
 ) {
+
+    private val logger = LoggerFactory.getLogger(TemperatureController::class.java)
 
     @GetMapping(produces = arrayOf(TEXT_EVENT_STREAM_VALUE))
     fun sink(): Flux<Temperature> =
@@ -22,5 +27,13 @@ open class TemperatureController(
             serverSentEventChannel.subscribe(handler)
         })
 
-//    TODO: handle the broken pipe errors when someone unsubscribes
+    @ExceptionHandler(IOException::class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    fun exceptionHandler(e: IOException, request: HttpServletRequest) =
+        if (e.message == "Broken pipe") {
+            logger.info("Client has disconnected")
+            null
+        } else {
+            HttpEntity(e.message)
+        }
 }
